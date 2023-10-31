a06fd8175c1a9c16fae4f79869b3af71fd247a64
Timur <tima97855@gmail.com>
2023-10-31 18:50:54 +0600

    Task_1

---
+++ Tasks.js
@@ -0,0 +1,22 @@
+'use strict'
+
+// 1) На forEach:
+
+// - Дан массив с числами. Создайте новый массив, состоящий из квадратов этих чисел
+
+// - Дан массив с числами. Найдите сумму этих чисел.
+
+// let arr = [1, 2, 3, 4, 5];
+// let sum = 0;
+
+// arr.forEach(function(elem) {
+// 	sum += elem;
+// });
+
+// console.log(sum);
+// const arr = [1, 2, 3, 4, 5];
+// const squares = arr.map(function(elem){
+//     return elem*elem
+// });
+// console.log(squares);
+
